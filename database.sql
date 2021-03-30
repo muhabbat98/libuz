6134a89b805ae03732263efbe85f2d026920bea5
muhabbat98 <bmpmuhabbatpulatova98@gmail.com>
2021-03-30 09:08:27 +0500

    fixed errors

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
-create extention pgcrypto;
+create extension pgcrypto;
 -- 01 users 
 create table users(
     user_id serial primary key,
@@ -9,16 +9,16 @@ create table users(
 -- 02 libraries 
 create table libraries(
     library_id serial primary key,
-    library_name varchar(64) not null,
+    library_name varchar(64) not null
 );
 -- 03 librarians 
 create table librarians(
     librarian_id serial primary key,
-    user_id int not null reference users(user_id),
+    user_id int not null references users(user_id),
     first_name varchar(40) not null,
     last_name varchar(40) not null,
     librarian_phone varchar(13) not null,
-    library_id int reference libraries(librarian_id)
+    library_id int references libraries(library_id)
 );
 
 --  04 reader roles  
@@ -29,9 +29,9 @@ create table reader_roles(
 -- 05 reader 
 create table readers(
     reader_id serial primary key,
-    user_id int reference users(user_id),
+    user_id int references users(user_id),
     reader_email varchar(64),
-    reader_role int reference reader_roles(reader_role_id),
+    reader_role int references reader_roles(reader_role_id),
     reader_phone varchar(13)
 );
 -- 06 themes of questions 
@@ -42,9 +42,9 @@ create table themes (
 -- 07 questions 
 create table questions(
     question_id serial primary key,
-    reader_id int reference readers(reader_id),
-    theme_id int reference themes(theme_id),
-    library_id int reference libraries(library_id),
+    reader_id int references readers(reader_id),
+    theme_id int references themes(theme_id),
+    library_id int references libraries(library_id),
     question_text text not null,
     question_status smallint default 1,
     created_at timestamptz default current_timestamp 
@@ -53,16 +53,22 @@ create table questions(
 -- 08 rooms 
 create table rooms (
     room_id serial primary key,
-    librarian_id int reference librarians(librarian_id),
-    reader_id int reference readers(reader_id)
+    librarian_id int references librarians(librarian_id),
+    reader_id int references readers(reader_id)
 );
 
 -- 09 answers
 create table answers(
     answer_id serial primary key,
-    question_id int reference questions(question_id),
-    librarian_id int reference librarians(librarian_id),
-    room_id int reference rooms(room_id),
+    question_id int references questions(question_id),
+    librarian_id int references librarians(librarian_id),
+    room_id int references rooms(room_id),
     answer_text varchar not null,
     created_at timestamptz default current_timestamp
+);
+--10 likes
+create table likes(
+    like_id serial primary key,
+    answer_id int references answers(answer_id),
+    reader_id int references readers(reader_id)
 );
